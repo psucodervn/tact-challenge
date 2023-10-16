ce99ada377c0f731989fe4a4bc539ad9449743f2
psucodervn <hungle.info@gmail.com>
2023-10-16 16:26:44 +0700

    feat: add Task1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,6 +1,7 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
 import { Task1 } from '../wrappers/Task1';
+import '@ton-community/test-utils';
 
 describe('Task1', () => {
     let blockchain: Blockchain;
@@ -29,5 +30,31 @@ describe('Task1', () => {
     });
 
     it('test', async () => {
+        expect(await task1.getCounter()).toBe(0n);
+        const caller = await blockchain.treasury('caller');
+
+        await task1.send(caller.getSender(), { value: toNano('0.05') }, { $$type: 'Add', queryId: 0n, number: 10n });
+        expect(await task1.getCounter()).toBe(10n);
+
+        await task1.send(
+            caller.getSender(),
+            { value: toNano('0.05') },
+            { $$type: 'Subtract', queryId: 0n, number: 5n }
+        );
+        expect(await task1.getCounter()).toBe(5n);
+
+        await task1.send(
+            caller.getSender(),
+            { value: toNano('0.05') },
+            { $$type: 'Subtract', queryId: 0n, number: 20n }
+        );
+        expect(await task1.getCounter()).toBe(-15n);
+
+        await task1.send(
+            caller.getSender(),
+            { value: toNano('0.05') },
+            { $$type: 'Add', queryId: 0n, number: 2_000_000_000n }
+        );
+        expect(await task1.getCounter()).toBe(1_999_999_985n);
     });
 });
