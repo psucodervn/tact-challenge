c4b025ae0d4ad867d827a6ef8e9d12b6068123a4
psucodervn <hungle.info@gmail.com>
2023-10-17 15:36:05 +0700

    try Task3

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -1,14 +1,20 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
+import { toNano, Address, Cell } from 'ton-core';
 import { Task3 } from '../wrappers/Task3';
+import '@ton-community/test-utils';
 
 describe('Task3', () => {
     let blockchain: Blockchain;
     let task3: SandboxContract<Task3>;
+    let admin: SandboxContract<TreasuryContract>;
+    let tokenA: Address = Address.parseRaw('0:0000000000000000000000000000000000000000000000000000000000000001');
+    let tokenB: Address = Address.parseRaw('0:0000000000000000000000000000000000000000000000000000000000000002');
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        task3 = blockchain.openContract(await Task3.fromInit());
+        admin = await blockchain.treasury('admin');
+        task3 = blockchain.openContract(await Task3.fromInit(admin.address, tokenA, tokenB));
+
         const deployer = await blockchain.treasury('deployer');
         const deployResult = await task3.send(
             deployer.getSender(),
@@ -26,10 +32,36 @@ describe('Task3', () => {
             deploy: true,
             success: true,
         });
+
+        await task3.send(
+            admin.getSender(),
+            { value: toNano('0.05') },
+            {
+                $$type: 'TokenNotification',
+                queryId: 0n,
+                amount: toNano('10'),
+                from: tokenA,
+                forwardPayload: new Cell(),
+            }
+        );
+        await task3.send(
+            admin.getSender(),
+            { value: toNano('0.05') },
+            {
+                $$type: 'TokenNotification',
+                queryId: 0n,
+                amount: toNano('2'),
+                from: tokenB,
+                forwardPayload: new Cell(),
+            }
+        );
     });
 
-    it('test', async () => {
+    it('test price', async () => {
+        const priceA = await task3.getPrice(tokenA);
+        expect(priceA).toEqual(5n * 10n ** 9n);
+
+        const priceB = await task3.getPrice(tokenB);
+        expect(priceB).toEqual(2n * 10n ** 8n);
     });
 });
-
-
